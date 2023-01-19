962f71b0d9740f2128fb2bba2c4cb50a5ece7ec9
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-01-19 11:38:43 +0100

    generate_reflection_headers: stop applying to headers only

--- scripts/generate_reflection_headers.cmake
+++ scripts/generate_reflection_headers.cmake
@@ -15,40 +15,31 @@ function(putils_generate_reflection_headers)
     set(python_script ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/generate_reflection_headers.py)
     putils_build_clang_arguments(clang_args ${ARGUMENTS_TARGET} ${ARGUMENTS_CLANG_ARGS})
 
-    # Get only header files
-    set(headers)
-    foreach(file ${ARGUMENTS_SOURCES})
-        get_filename_component(extension ${file} EXT)
-        if (${extension} STREQUAL ".hpp")
-            list(APPEND headers ${file})
-        endif()
-    endforeach()
-
     # Add a command for each reflection header we need to generate
-    # This lets us have "atomic" commands that only run when their specific header is modified
+    # This lets us have "atomic" commands that only run when their specific source file is modified
     # Each command will create a "time_marker_file" used as a timestamp for the last time reflection code was generated
     set(time_marker_files)
-    foreach(header ${headers})
-        set(command ${PYTHON_EXECUTABLE} ${python_script} ${header} --clang-args ${clang_args})
+    foreach(source_file ${ARGUMENTS_SOURCES})
+        set(command python ${python_script} ${source_file} --clang-args ${clang_args})
         if (${ARGUMENTS_EXTENSION})
             list(APPEND command --extension ${ARGUMENTS_EXTENSION})
         endif()
 
-        # Dummy file to avoid re-running the command if `header` hasn't changed
+        # Dummy file to avoid re-running the command if `source_file` hasn't changed
         get_target_property(binary_dir ${ARGUMENTS_TARGET} BINARY_DIR)
         get_target_property(source_dir ${ARGUMENTS_TARGET} SOURCE_DIR)
-        file(RELATIVE_PATH header_relative_path ${source_dir} ${header})
+        file(RELATIVE_PATH header_relative_path ${source_dir} ${source_file})
         set(time_marker_file ${binary_dir}/${header_relative_path}.generated_reflection)
 
         get_filename_component(time_marker_directory ${time_marker_file} DIRECTORY)
 
         add_custom_command(
                 OUTPUT ${time_marker_file}
-                COMMENT "Generating reflection code for ${header}"
+                COMMENT "Generating reflection code for ${source_file}"
                 COMMAND ${command}
                 COMMAND ${CMAKE_COMMAND} -E make_directory ${time_marker_directory}
                 COMMAND ${CMAKE_COMMAND} -E touch ${time_marker_file}
-                DEPENDS ${header} ${python_script}
+                DEPENDS ${source_file} ${python_script}
         )
         list(APPEND time_marker_files ${time_marker_file})
     endforeach()
